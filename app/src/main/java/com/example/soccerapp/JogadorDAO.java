52a38442a8c958e61c519a6bade5a453288dddc4
LeonardoCesca <leonardocescaf@gmail.com>
2019-09-11 22:57:27 -0300

    ADD method inserirJogador into JogadorDAO

---
+++ app/src/main/java/com/example/soccerapp/JogadorDAO.java
@@ -0,0 +1,21 @@
+package com.example.soccerapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class JogadorDAO {
+
+    public static void inserirJogador(Context contexto, Jogador jogador) {
+        Banco banco = new Banco(contexto);
+        SQLiteDatabase database = banco.getWritableDatabase();
+
+        ContentValues valoresCampos = new ContentValues();
+        valoresCampos.put("nome", jogador.getNome());
+        valoresCampos.put("apelido", jogador.getApelido());
+        valoresCampos.put("numCamiseta", jogador.getNumeroCamiseta());
+
+        database.insert("jogadores", null, valoresCampos);
+    }
+}
