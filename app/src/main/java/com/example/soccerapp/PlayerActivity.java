5df1defab9f98af40bff14459023699cbd57ee1f
LeonardoCesca <leonardocescaf@gmail.com>
2019-09-11 22:49:01 -0300

    ADD method salvar on the PlayerActivity

--- app/src/main/java/com/example/soccerapp/PlayerActivity.java
+++ app/src/main/java/com/example/soccerapp/PlayerActivity.java
@@ -5,17 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PlayerActivity extends AppCompatActivity {
+
+    private EditText edtNome, edtApelido, edtNumCamiseta;
+    private Button btnSalvarJogador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
-        //Back Button
+        edtNome = (EditText) findViewById(R.id.nomeJogador);
+        edtApelido = (EditText) findViewById(R.id.apelidoJogador);
+        edtNumCamiseta = (EditText) findViewById(R.id.numeroCamiseta);
+        btnSalvarJogador = (Button) findViewById(R.id.salvarJogadorBtn);
+
+        btnSalvarJogador.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                salvar();
+            }
+        });
+
+        //Botão de voltar
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void salvar() {
+        String nomeJogador = edtNome.getText().toString();
+        String apelidoJogador = edtApelido.getText().toString();
+        String numCamiseta = edtNumCamiseta.getText().toString();
     }
 
     @Override
