8f494ecb09241bc155e5cf79ab9b1d47e5a70cbb
LeonardoCesca <leonardocescaf@gmail.com>
2019-09-11 22:50:25 -0300

    ADD Jogador class with attributes and methods

---
+++ app/src/main/java/com/example/soccerapp/Jogador.java
@@ -0,0 +1,42 @@
+package com.example.soccerapp;
+
+public class Jogador {
+
+    private int id;
+    private String nome;
+    private String apelido;
+    private int numeroCamiseta;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public int getNumeroCamiseta() {
+        return numeroCamiseta;
+    }
+
+    public void setNumeroCamiseta(int numeroCamiseta) {
+        this.numeroCamiseta = numeroCamiseta;
+    }
+}
