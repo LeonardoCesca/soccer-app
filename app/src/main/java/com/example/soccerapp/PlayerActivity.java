93277ac356d000dd1ce57414851b5424f40d5f78
LeonardoCesca <leonardocescaf@gmail.com>
2019-09-11 21:44:28 -0300

    REMOVE AppBarLayout into Main Activity

--- app/src/main/java/com/example/soccerapp/PlayerActivity.java
+++ app/src/main/java/com/example/soccerapp/PlayerActivity.java
@@ -10,5 +10,7 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+
     }
 }
