972bc16863ce92936f9e21bb1b4d24685223dc5c
LeonardoCesca <leonardocescaf@gmail.com>
2019-09-11 21:52:49 -0300

    ADD back arrow previous page

--- app/src/main/java/com/example/soccerapp/PlayerActivity.java
+++ app/src/main/java/com/example/soccerapp/PlayerActivity.java
@@ -1,8 +1,10 @@
 package com.example.soccerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class PlayerActivity extends AppCompatActivity {
 
@@ -11,6 +13,20 @@ public class PlayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
+        //Back Button
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if(id == android.R.id.home) {
+            this.finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
