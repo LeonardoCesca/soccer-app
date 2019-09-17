d1e3a492d68132d2e969353a15ef34ddd305a81b
Lucas Zoppas <lucasmzoppas@gmail.com>
2019-09-17 18:46:50 -0300

    ADD class and layout, Time and activity lista

---
+++ app/src/main/java/com/example/soccerapp/Time.java
@@ -0,0 +1,33 @@
+package com.example.soccerapp;
+
+public class Time {
+
+    private String nomeTime, anoFundacao;
+    private int id;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNomeTime() {
+        return nomeTime;
+    }
+
+    public void setNomeTime(String nomeTime) {
+        this.nomeTime = nomeTime;
+    }
+
+    public String getAnoFundacao() {
+        return anoFundacao;
+    }
+
+    public void setAnoFundacao(String anoFundacao) {
+        this.anoFundacao = anoFundacao;
+    }
+
+
+}
